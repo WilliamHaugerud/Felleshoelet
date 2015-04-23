79ba84960a8d491bb53b7adb56d18b9eaf3e1435
Vemund Santi <vemund@santi.no>
2015-04-23 20:10:56 +0200

    Added TypeWriter.ino, the code for the Arduino

---
+++ src/TypeWriter/TypeWriter.ino
@@ -0,0 +1,51 @@
+
+int inputPin = 8;
+int ledPin = 9;
+
+int lastState = HIGH ;
+int currentState = HIGH;
+int current = LOW;
+
+void setup(){
+  pinMode(inputPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop(){
+  current = digitalRead(inputPin);
+  int temp = LOW;
+  if (current == HIGH){
+   while (current == HIGH){
+      current = digitalRead(inputPin);
+      delay(30);
+      temp = digitalRead(inputPin);
+      if (temp == LOW){
+        delay(30);
+        temp = digitalRead(inputPin);
+        if (temp == LOW){
+          current = LOW;
+        } else {
+         current = HIGH;
+        }
+      }
+      Serial.println("ARMED AND READY");
+    }
+   Serial.println("PLING");
+   delay(2000);
+  }
+  /*
+  currentState = digitalRead(inputPin);
+  if(lastState == HIGH){
+    if(currentState == LOW){
+      digitalWrite(ledPin, HIGH);
+      Serial.println("PLING!");
+      delay(4000);
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  Serial.println("Venter på kontakt");
+  lastState = currentState;
+  */
+}
