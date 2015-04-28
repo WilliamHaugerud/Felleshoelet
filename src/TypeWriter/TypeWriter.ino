3ae1231ea856b6f07d771aa73e7fa3b3046c41c1
Kristian Rekstad <krirek@msn.com>
2015-04-28 20:31:26 +0200

    Added speaker pins. Tweaked delay times. Moved some stuff to defines.

--- src/TypeWriter/TypeWriter.ino
+++ src/TypeWriter/TypeWriter.ino
@@ -1,15 +1,26 @@
 
 int inputPin = 8;
-int ledPin = 9;
+int relayPin = 9;
+int speakerPin = 3;
 
-#define PLING_TIME 10
+#define RELAY_TIME 10
+#define SPEAKER_TIME 3
+
+#define RELAY_ACTIVE LOW
+#define RELAY_OFF HIGH
+
+#define SPEAKER_ON HIGH
+#define SPEAKER_OFF LOW
 
 int current = LOW;
 
 void setup(){
   pinMode(inputPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  pinMode(relayPin, OUTPUT);
+  pinMode(speakerPin, OUTPUT);
+
+  digitalWrite(relayPin, RELAY_OFF);
+  digitalWrite(speakerPin, SPEAKER_OFF);
 }
 
 
@@ -20,10 +31,10 @@ void loop(){
   if (current == HIGH){
    while (current == HIGH){
       current = digitalRead(inputPin);
-      delay(30);
+      delay(25);
       temp = digitalRead(inputPin);
       if (temp == LOW){
-        delay(30);
+        delay(25);
         temp = digitalRead(inputPin);
         if (temp == LOW){
           current = LOW;
@@ -33,9 +44,14 @@ void loop(){
       }
         //ARMED
     }
-    digitalWrite(ledPin, LOW);
+
+    // TRIGGER THE STUFFZ
+    digitalWrite(relayPin, RELAY_ACTIVE);
+    digitalWrite(speakerPin, SPEAKER_ON);
+    delay(SPEAKER_TIME*1000);
+    digitalWrite(speakerPin, SPEAKER_OFF);
     //PLING
-   delay(PLING_TIME*1000);
-   digitalWrite(ledPin, HIGH);
+    delay((RELAY_TIME - SPEAKER_TIME) *1000);
+    digitalWrite(relayPin, RELAY_OFF);
   }
 }
