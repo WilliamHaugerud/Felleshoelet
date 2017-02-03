9add3802b7ac69f5d92e5183627b68eeac0e56f4
Simon B-J <simon_bj@live.com>
2017-02-03 16:14:09 +0100

    Updated to match Leonardo

--- src/Felleshoel/Felleshoel.ino
+++ src/Felleshoel/Felleshoel.ino
@@ -1,16 +1,16 @@
 #define DETECTOR_ACTIVE_STATE 0 // State when detector is active
 
-#define RELAY_ON 1 // State when relay is activated
+#define LIGHT_ON 1 // State when light is activated
 #define SPEAKER_ON 0 // State when speaker is activated
 
-#define RELAY_DURATION 5000 // Duration of alarm lights
+#define LIGHT_DURATION 5000 // Duration of alarm lights
 #define SPEAKER_DURATION 3000 // Duration of alarm sound
 
-#define RELAY_PIN 13 // Output pin for relay
-#define SPEAKER_PIN 12 // Output pin for speaker
-#define DETECTOR_PIN 11 // Input pin for detector
+#define LIGHT_PIN 9 // Output pin for light
+#define SPEAKER_PIN 7 // Output pin for speaker
+#define DETECTOR_PIN 4 // Input pin for detector
 
-
+int lightOnlyDuration = LIGHT_DURATION - SPEAKER_DURATION;
 boolean alarmLock = false;
 
 
@@ -24,16 +24,16 @@ void activateAlarm(){
     
     alarmLock = true;
     
-    digitalWrite(RELAY_PIN, RELAY_ON);
+    digitalWrite(LIGHT_PIN, LIGHT_ON);
     digitalWrite(SPEAKER_PIN, SPEAKER_ON);
     
     delay(SPEAKER_DURATION);
     
     digitalWrite(SPEAKER_PIN, !SPEAKER_ON);
     
-    delay(abs(RELAY_DURATION - SPEAKER_DURATION));
+    delay(lightOnlyDuration);
     
-    digitalWrite(RELAY_PIN, !RELAY_ON);
+    digitalWrite(LIGHT_PIN, !LIGHT_ON);
     
     alarmLock = false;
 }
@@ -48,14 +48,12 @@ boolean checkCircuit(){
 
 
 void setup() {
-    pinMode(RELAY_PIN, OUTPUT); // Set pin controlling relay as output
+    pinMode(LIGHT_PIN, OUTPUT); // Set pin controlling light as output
     pinMode(SPEAKER_PIN, OUTPUT); // Set pin controlling speaker as output
     pinMode(DETECTOR_PIN, INPUT); // Set pin for detecting closed circuit as input
     
-    digitalWrite(RELAY_PIN, !RELAY_ON);
+    digitalWrite(LIGHT_PIN, !LIGHT_ON);
     digitalWrite(SPEAKER_PIN, !SPEAKER_ON);
-    
-    Serial.begin(9600);
 }
 
 
